235d6a7a2fec0e09fbbac43e97fcde6007456127
shereenwalid <93673791+shereenwalid@users.noreply.github.com>
2023-12-07 21:01:18 +0200

    Update RegistryFactory.java

--- src/Factory/RegistryFactory.java
+++ src/Factory/RegistryFactory.java
@@ -3,10 +3,6 @@ import Factory.Registry.*;
 import Factory.Item.*;
 public class RegistryFactory {
     public I_ItemRegistry getCategoryRegistry(String categoryName) {
-        if (categoryName == null) {
-            throw new IllegalArgumentException("Category name cannot be null");
-        }
-
         if (categoryName.equalsIgnoreCase("Books") || categoryName.equalsIgnoreCase("Book")) {
             return new BookRegistry();
         } else if (categoryName.equalsIgnoreCase("Electronics") || categoryName.equalsIgnoreCase("Electronic")) {
