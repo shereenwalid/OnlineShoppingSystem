8ef4d47cef4a91a027e545f77cfab5a57d47a9ae
shereenwalid <93673791+shereenwalid@users.noreply.github.com>
2023-12-19 19:25:05 +0200

    Update DataBaseConnection.java

--- src/Database/DataBaseConnection.java
+++ src/Database/DataBaseConnection.java
@@ -12,13 +12,11 @@ public class DataBaseConnection {
     private DataBaseConnection() {
     }
 
-    // Method to get a connection using Singleton pattern
     public static synchronized Connection getConnection() {
         if (connection == null) {
             try {
                 connection = DriverManager.getConnection(DB_URL);
             } catch (SQLException e) {
-                // Log or handle the exception appropriately
                 e.printStackTrace();
                 throw new RuntimeException("Error connecting to the database.", e);
             }
@@ -26,16 +24,14 @@ public class DataBaseConnection {
         return connection;
     }
 
-    // Method to close the connection
     public static synchronized void closeConnection() {
         if (connection != null) {
             try {
                 connection.close();
             } catch (SQLException e) {
-                // Log or handle the exception appropriately
                 e.printStackTrace();
             } finally {
-                connection = null; // Set connection to null after closing
+                connection = null;
             }
         }
     }
