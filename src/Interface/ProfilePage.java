ca757e8d9da10066cc6afe99e28de35776de4d1b
shereenwalid <93673791+shereenwalid@users.noreply.github.com>
2023-12-19 19:22:11 +0200

    Update ProfilePage.java

--- src/Interface/ProfilePage.java
+++ src/Interface/ProfilePage.java
@@ -17,7 +17,7 @@ public class ProfilePage extends JFrame {
 //    private JTextField balanceField;
 //    private JTextField expiryDateField;
     Buyer buyer;
-//    To update information
+
 public ProfilePage(Buyer buyer) {
     JPanel navbar = new JPanel();
     navbar.setBackground(Color.white);
@@ -33,7 +33,6 @@ public ProfilePage(Buyer buyer) {
     setTitle("Profile Page");
     setSize(800, 600);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    // Center the frame on the screen
     setLocationRelativeTo(null);
 
     JPanel panel = new JPanel();
