cb1c7415b424f4036d893bb9017776ae33e6a49a
shereenwalid <93673791+shereenwalid@users.noreply.github.com>
2023-12-21 22:54:38 +0200

    Delete src directory

--- src/Factory/Item/Book.java
+++
@@ -1,7 +0,0 @@
-package Factory.Item;
-
-public class Book extends A_Item {
-    public Book(int bookID, String title, double price, int quantity, String description, int salePercent, int categoryID, int itemDeliveryDuration) {
-        super(bookID, title, price, quantity, description, salePercent, categoryID, itemDeliveryDuration);
-    }
-}
