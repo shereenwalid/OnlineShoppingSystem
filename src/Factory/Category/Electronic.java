73bf9cd2e3a5848bf1ff04a3bb1795a642849d9a
shereenwalid <93673791+shereenwalid@users.noreply.github.com>
2023-12-23 15:38:54 +0200

    Delete src directory

--- src/Factory/Category/Electronic.java
+++
@@ -1,7 +0,0 @@
-package Factory.Category;
-
-public class Electronic extends A_Item {
-    public Electronic(int electronicID, String itemName, double price, int quantity, String description, int salePercent, int categoryID,int itemDeliveryDuration) {
-        super(electronicID, itemName, price, quantity, description, salePercent, categoryID,itemDeliveryDuration);
-    }
-}
